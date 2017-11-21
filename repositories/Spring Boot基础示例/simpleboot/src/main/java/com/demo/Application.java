712d2878a69fdc3c05c6bb653aa0e15717535a5d
yu-linfeng <hellobug@outlook.com>
2017-11-21 19:26:37 +0800

    Spring Boot基础示例

---
+++ repositories/Spring Boot基础示例/simpleboot/src/main/java/com/demo/Application.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    String index() {
+        return "boot";
+    }
+
+
+
+    public static void main( String[] args ) {
+        SpringApplication.run(Application.class, args);
+    }
+}
