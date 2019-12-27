8bea2e4b1ae6fbd868314cf4ba79bccc5a636b94
yulinfeng <yulinfeng@nibirutech.com>
2019-12-28 00:43:21 +0800

    如何更优雅地对接第三方API

---
+++ repositories/third/third-2-resttemplate-prop/src/main/java/com/coderbuff/third2resttemplateprop/Third2RestTemplatePropApplication.java
@@ -0,0 +1,13 @@
+package com.coderbuff.third2resttemplateprop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Third2RestTemplatePropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Third2RestTemplatePropApplication.class, args);
+    }
+
+}
---
+++ repositories/third/third-3-feign-prop/src/main/java/com/coderbuff/third3feignprop/entity/JuheStockResult.java
@@ -0,0 +1,12 @@
+package com.coderbuff.third3feignprop.entity;
+
+import lombok.Data;
+
+/**
+ * @author yulinfeng
+ * @date 2019/12/26
+ */
+@Data
+public class JuheStockResult {
+    private JuheStockResultDapanData dapandata;
+}
